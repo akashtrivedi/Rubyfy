9fdde17ff251ee07ef3126a39fdb8591b97f13e9
Akash Trivedi <dev202@outlook.com>
2014-04-05 03:17:45 -0500

    renamed folder page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rubyfy::Application.routes.draw do
+root "pages#home" 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
