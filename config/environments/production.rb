716599087787b4da5a754ff85402177a1e372797
Akash Trivedi <dev202@outlook.com>
2014-04-06 20:43:07 -0500

    imeplemented devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Rubyfy::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #required for heroku-mail
+  #set it to actual hostname
+  config.action_mailer.default_url_options = { host: 'rubyfy.herokuapp.com' }
 end
